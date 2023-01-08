862c99706b56e0d54864b84e66a3d2d8f4d99cd8
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-01-08 10:35:30 +0100

    add scripts/txapply.sh

---
+++ scripts/txapply.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cat website.csv | while read l; do
+	txid=$(echo "$l" | cut -d, -f 3)
+
+	if [ "$txid" != "" ]; then
+		id=$(echo "$l" | cut -d, -f 1)
+		slug=$(echo "$l" | cut -d, -f 2)
+		path="$(printf 'website/%03i-%s' "$id" "$slug")"
+		txpath="transifex/$txid"
+
+		if [ "$1" = "-v" ]; then
+			echo "apply $path" >&2
+			rm -rf "$txpath"
+			cp -r "$path" "$txpath"
+		else
+			echo "apply $txpath" >&2
+			rm -rf "$path"
+			cp -r "$txpath" "$path"
+		fi
+	fi
+done
